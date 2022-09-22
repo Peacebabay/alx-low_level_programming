30f2f9723ed5c50f97ab238a7bf511975dd74635
peace <edwardpeacendidiamaka@gmail.com>
2022-09-22 15:55:55 -0700

    My sixth commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,36 +1,34 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize words
+ * cap_string - capitalize word
  * @s: pointer
  * Return: capitalzied string
- * /
+ */
 
 char *cap_string(char *s)
-
 {
-int count = 0, i;
-int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
-if (*(s + count) >= 97 && *(s + count) <= 122)
-*(s + count) = *(s + count) - 32;
-count++;
+	int count = 0, i;
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-while (*(s + count) != '\0')
-{
-for (i = 0; i < 13; i++)
-{
-if (*(s + count) == separators[i])
-{
-if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
- *(s + (count + 1)) = *(s + (count + 1)) - 32;
- break;
- }
- 
+	if (*(s + count) >= 97 && *(s + count) <= 122)
+		*(s + count) = *(s + count) - 32;
+	count++;
+	while (*(s + count) != '\0')
+	{
+		for (i = 0; i < 13; i++)
+		{
+			if (*(s + count) == separators[i])
+			{
+				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
+					*(s + (count + 1)) = *(s + (count + 1)) - 32;
+				break;
+			}
+		}
 
-}
-count++;
-}
+		count++;
+	}
 
-return (s);
+	return (s);
 
 }
