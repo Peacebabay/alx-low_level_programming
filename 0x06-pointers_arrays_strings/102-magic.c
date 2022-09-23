f9ac9dfb6e8fa00a188d19b81e4f70f6af8dd00e
peace <edwardpeacendidiamaka@gmail.com>
2022-09-23 05:03:58 -0700

    My tenth commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -9,6 +9,7 @@ int main(void);
 
 	a[2] = 1024;
 	p = &n;
+
 	/**
 	 * write your line of code here...
 	 * - you are not allowed to use a
@@ -17,6 +18,7 @@ int main(void);
 	 *
 	 *
 	 */
+
 	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
 
