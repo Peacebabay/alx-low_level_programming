8b2a2516fb1ec8e438701d591750ed6ee10be473
peace <edwardpeacendidiamaka@gmail.com>
2022-09-23 04:57:10 -0700

    My tenth commit

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -13,6 +13,9 @@ int main(void);
 	 * write your line of code here...
 	 * - you are not allowed to use a
 	 *   - you are not allowed to modify p
+	 *
+	 *
+	 *
 	 */
 	p[5] = 98;
 	printf("a[2] = %d\n", a[2]);
